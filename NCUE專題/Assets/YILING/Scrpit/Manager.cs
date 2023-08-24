958923d66b0ec551a06aca14a66cb5907f10d063
SelinaJoJo <tsengselina0914@gmail.com>
2023-08-24 23:56:53 +0800

    YILING-UI_2

---
+++ NCUE專題/Assets/YILING/Scrpit/Manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName); // 使用 SceneManager.LoadScene 加载场景
+    }
+
+    public void GoBack()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex - 1);
+    }
+}
